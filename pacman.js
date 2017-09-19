2bbb6c22d8a713502f7f67edd51a4ef38f4aa12c
maryfranks <maryfranks@gmail.com>
2017-09-19 09:46:57 -0400

    update game dialog

--- pacman.js
+++ pacman.js
@@ -1,10 +1,8 @@
-// Setup initial game stats
+
 var score = 0;
 var lives = 2;
 var powerPellets = 4;
 
-
-// Define your ghosts here
 var inky = {
   menu_option: '1',
   name: 'Inky',
@@ -25,7 +23,7 @@ var pinky = {
   menu_option: '3',
   name: 'Pinky',
   colour: 'Pink',
-  character: 'Bashful',
+  character: 'Bashfull',
   edible: false
 };
 
@@ -44,6 +42,7 @@ var ghosts = [inky, blinky, pinky, clyde]
 function drawScreen() {
   clearScreen();
   setTimeout(function() {
+    welcomeToPacman()
     displayStats();
     displayMenu();
     displayPrompt();
@@ -54,13 +53,17 @@ function clearScreen() {
   console.log('\x1Bc');
 }
 
+function welcomeToPacman() {
+  console.log('Welcome to Pacman\n');
+  console.log('Pacman is hungry and wants to eat as many dots as he can,\nbut be careful of ghosts!\nYou can eat ghosts for extra points if you have power pellets.\n')
+}
+
 function displayStats() {
-  console.log('Score: ' + score + '     Lives: ' + lives);
-  console.log('Power Pellets: ' + powerPellets)
+  console.log('Score: ' + score + '     Lives: ' + lives + '     Power Pellets: ' + powerPellets);
 }
 
 function displayMenu() {
-  console.log('\n\nSelect Option:\n');  // each \n creates a new line
+  console.log('\nSelect Option:\n');
   console.log('(d) Eat Dot');
   for (var i = 0; i < ghosts.length; i++) {
     console.log("(" + ghosts[i]['menu_option'] + ") Eat " + ghosts[i]['name'] + "(" + displayGhostEdible(ghosts[i]) + ")");
@@ -80,10 +83,8 @@ function displayGhostEdible(ghost) {
 }
 
 function displayPrompt() {
-  // process.stdout.write is similar to console.log except it doesn't add a new line after the text
   process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
 }
-
 
 // Menu Options
 function eatDot() {
@@ -113,6 +114,7 @@ function loseLife() {
 
 function eatPowerPellet() {
   if (powerPellets > 0) {
+    console.log("\nYou ate a Power Pellet!\nHurry up and eat as many ghosts as you can!");
     score += 50;
     for (var i = 0; i < ghosts.length; i++) {
       ghosts[i]['edible'] = true
@@ -122,7 +124,6 @@ function eatPowerPellet() {
     console.log("\nYou are out of Power Pellets!")
   }
 }
-
 
 // Process Player's Input
 function processInput(key) {
@@ -154,11 +155,6 @@ function processInput(key) {
   }
 }
 
-
-//
-// YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
-//
-
 // Setup Input and Output to work nicely in our Terminal
 var stdin = process.stdin;
 stdin.setRawMode(true);
@@ -172,7 +168,7 @@ drawScreen();
 stdin.on('data', function(key) {
   process.stdout.write(key);
   processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+  setTimeout(drawScreen, 800);
 });
 
 // Player Quits
